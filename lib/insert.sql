f274aba8ff6851ed63235c9e376cab504f0db209
KellyAineah <kellyainea99@gmail.com>
2024-06-08 22:42:48 +0300

    added columns

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 22, 'M', "black", "high", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy", 25, 'M', "black", "high", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 22, 'M', "black", "medium", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 42, 'F', "white", "low", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 35, 'M', "white", "high", 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch", 22, 'M', "black", "high", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy", 52, 'F', "black", "high", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 22, 'M', "black", "high", 0);
